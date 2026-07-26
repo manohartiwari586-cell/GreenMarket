43e8455fe2201dc7eada34a9d99a3d102b2abc5f
Manohar <manohartiwari586@gmail.com>
2026-07-26 15:40:12 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,216 @@
+let cartCount = 0;
+
+const cartButtons = document.querySelectorAll(".cartBtn");
+
+cartButtons.forEach(button=>{
+
+button.addEventListener("click",function(){
+
+let cart=JSON.parse(localStorage.getItem("cart")) || [];
+
+let product={
+
+name:this.dataset.name,
+
+price:this.dataset.price,
+
+image:this.dataset.image,
+
+qty:1
+
+};
+
+cart.push(product);
+
+localStorage.setItem("cart",JSON.stringify(cart));
+
+cartCount=cart.length;
+
+document.getElementById("cartCount").innerHTML=cartCount;
+
+document.getElementById("floatingCount").innerHTML=cartCount;
+
+alert("✅ Product Added To Cart");
+
+});
+
+});
+
+// ----------------------
+// Quantity
+// ----------------------
+
+const qtyBoxes=document.querySelectorAll(".qty");
+
+qtyBoxes.forEach(box=>{
+
+let minus=box.children[0];
+let number=box.children[1];
+let plus=box.children[2];
+
+let qty=1;
+
+plus.onclick=function(){
+
+qty++;
+
+number.innerHTML=qty;
+
+};
+
+minus.onclick=function(){
+
+if(qty>1){
+
+qty--;
+
+number.innerHTML=qty;
+
+}
+
+};
+
+});
+
+// ----------------------
+// Search
+// ----------------------
+
+const search=document.getElementById("search");
+
+search.addEventListener("keyup",function(){
+
+let value=search.value.toLowerCase();
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+let name=card.querySelector("h3").innerHTML.toLowerCase();
+
+if(name.includes(value)){
+
+card.style.display="block";
+
+}
+
+else{
+
+card.style.display="none";
+
+}
+
+});
+
+});
+
+// ----------------------
+// Category Filter
+// ----------------------
+
+const categoryButtons=document.querySelectorAll(".categories button");
+
+categoryButtons.forEach(button=>{
+
+button.addEventListener("click",function(){
+
+categoryButtons.forEach(btn=>btn.classList.remove("active"));
+
+this.classList.add("active");
+
+let category=this.innerText.toLowerCase();
+
+let cards=document.querySelectorAll(".card");
+
+cards.forEach(card=>{
+
+let product=card.querySelector("h3").innerText.toLowerCase();
+
+if(category=="all"){
+
+card.style.display="block";
+
+}
+
+else if(category.includes("vegetables")){
+
+if(product=="tomato" || product=="potato" || product=="onion"){
+
+card.style.display="block";
+
+}
+
+else{
+
+card.style.display="none";
+
+}
+
+}
+
+else if(category.includes("fruits")){
+
+if(product=="apple" || product=="banana"){
+
+card.style.display="block";
+
+}
+
+else{
+
+card.style.display="none";
+
+}
+
+}
+
+else if(category.includes("dairy")){
+
+if(product=="milk"){
+
+card.style.display="block";
+
+}
+
+else{
+
+card.style.display="none";
+
+}
+
+}
+
+else{
+
+card.style.display="block";
+
+}
+
+});
+
+});
+const hearts=document.querySelectorAll(".wishlist");
+
+hearts.forEach(function(heart){
+
+heart.onclick=function(){
+
+heart.classList.toggle("active");
+
+if(heart.classList.contains("active")){
+
+heart.innerHTML="❤️";
+
+}
+
+else{
+
+heart.innerHTML="♡";
+
+}
+
+};
+
+});
+
+});
